782df41df23c48d474cdb2d046bfe38d978f0dd6
MariaSilva24 <135022507+MariaSilva24@users.noreply.github.com>
2024-08-16 11:34:01 -0300

    aula

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const router = express.Router(); 
+
+// referência a controllers que serão utilizados nas rotas
+const UsuariosController = require('../controllers/usuarios'); 
+
+// definição das rotas
+// post
+// patch
+// delete
+router.get('/usuarios', UsuariosController.listarUsuarios); 
+router.post('/usuarios', UsuariosController.cadrastarUsuarios); 
+router.patch('/usuarios', UsuariosController.editarUsuarios); 
+router.delete('/usuarios', UsuariosController.apagarUsuarios); 
+
+
+module.exports = router;
